f862924f2a8f7e901e07ee2c870a1b7d5e55531b
alexandre-allari <113803011+alexandre-allari@users.noreply.github.com>
2024-11-08 14:48:55 +0000

    Je push mon code de allari alexandre

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from math import sqrt
-
-#### Fonction secondaire
-
-
+"""
+efze
+"""
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
+    """ 
+    Doc
+    """
+    if p <=1:
+        return False
+    for i in range(2,int(p**0.5)+1): ##Il suffit de tester jusqu'à la racine carré du p
+        if p % i ==0 :
+            return False
+    return True
 #### Fonction principale
 
-
 def main():
-
+    """
+        Doc
+        """
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
-
 if __name__ == "__main__":
     main()
